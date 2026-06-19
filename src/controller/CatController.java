cbea91e4412e917aec47e4579c8a78d90774c3eb
juyeungyung0930 <gyk09305@gmail.com>
2026-06-19 14:06:53 +0900

    feat:CatController.java 코드작성

--- src/controller/CatController.java
+++ src/controller/CatController.java
@@ -1,10 +1,17 @@
 package controller;
 
+import model.service.CatService;
+import view.CatView;
+
 /**
- * 작성자 :주연경
- * 작성일 :2026. 6. 19.
- * 설명   :CatController
+ * 고양이 캐릭터 생성/조회 흐름을 제어하는 컨트롤러
+ * 현재는 MainController에서 직접 처리하고 있으나
+ * 캐릭터 관련 기능이 늘어날 경우 이 클래스로 위임한다
  */
 public class CatController {
 
+    private CatService catService = new CatService();
+    private CatView    catView    = new CatView();
+
+    // TODO: 캐릭터 상세 조회, 수치 확인 등 기능 추가 예정
 }
