ef68526cd46b15ba645d004415f7b9984d888c23
ychun4012 <ychun4012@gmail.com>
2026-06-19 15:00:47 +0900

    feat: db id와 pw추가

--- src/common/DBUtil.java
+++ src/common/DBUtil.java
@@ -12,8 +12,8 @@ public class DBUtil {
 
     // Oracle DB 접속 정보 - 본인 환경에 맞게 수정
     private static final String URL      = "jdbc:oracle:thin:@localhost:1521:xe";
-    private static final String USER     = "your_username";
-    private static final String PASSWORD = "your_password";
+    private static final String USER     = "catwalk";
+    private static final String PASSWORD = "1234";
 
     static {
         try {
